b086e9a93b0d7ebec30b1de6a3013afc12e6944a
Jérémy Jourdan <github@kema.dev>
2025-07-09 14:12:07 +0200

    up

--- pkg/repotpl/repotpl.go
+++ pkg/repotpl/repotpl.go
@@ -6,8 +6,10 @@ import (
 	"net/url"
 	"os"
 	"os/exec"
+	"strings"
 	"syscall"
 
+	"github.com/kemadev/ci-cd/pkg/git"
 	"github.com/spf13/cobra"
 )
 
@@ -33,6 +35,36 @@ func Init(cmd *cobra.Command, args []string) error {
 	}
 
 	baseArgs := []string{"copy", RepoTemplateURL.String(), "."}
+
+	repo, err := git.GetGitBasePath()
+	if err != nil {
+		return fmt.Errorf("error getting git base path: %w", err)
+	}
+	if repo == "" {
+		return fmt.Errorf("not in a git repository, please run this command inside a git repository")
+	}
+	repoFull := "https://" + repo
+
+	repoUrl, err := url.Parse(repoFull)
+	if err != nil {
+		return fmt.Errorf("error parsing git repository URL: %w", err)
+	}
+
+	baseArgs = append(baseArgs, "--data", "vcs_url_scheme" + "=" + repoUrl.Scheme + "://")
+	baseArgs = append(baseArgs, "--data", "vcs_server_host" + "=" + repoUrl.Host)
+
+	vcsParts := strings.Split(strings.TrimPrefix(repoUrl.Path, "/"), "/")
+	if len(vcsParts) != 2 {
+		return fmt.Errorf("unexpected git repository URL format: %s", repoUrl.Path)
+	}
+
+	baseArgs = append(baseArgs, "--data", "vcs_namespace" + "=" + vcsParts[0])
+	baseArgs = append(baseArgs, "--data", "vcs_repo" + "=" + vcsParts[1])
+
+	if repoUrl.Host == "github.com" {
+		baseArgs = append(baseArgs, "--data", "vcs_provider" + "=" + "github")
+	}
+
 	slog.Debug("Running command", slog.Any("binary", binary), slog.Any("baseArgs", baseArgs))
 
 	err = syscall.Exec(binary, append([]string{binary}, baseArgs...), os.Environ())
