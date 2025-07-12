f91eb676ed0b7a13723634617a91e8ca5171fc04
Jérémy Jourdan <github@kema.dev>
2025-07-12 17:29:55 +0200

    feat: add silent mode

--- cmd/commands/log.go
+++ cmd/commands/log.go
@@ -10,5 +10,8 @@ func toggleDebug(cmd *cobra.Command, args []string) {
 	if cmd.Flag("debug").Value.String() == "true" {
 		slog.SetLogLoggerLevel(slog.LevelDebug)
 		slog.Debug("Debug mode is enabled, setting log level to debug")
+	} else if cmd.Flag("silent").Value.String() == "true" {
+		slog.SetLogLoggerLevel(slog.LevelError)
+		slog.Debug("Silent mode is enabled, setting log level to error")
 	}
 }
