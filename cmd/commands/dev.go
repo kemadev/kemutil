736ecf127ea81583a1cd2396945055d57d6a3bf8
Jérémy Jourdan <github@kema.dev>
2025-07-15 20:47:36 +0200

    feat: add dev stop

--- cmd/commands/dev.go
+++ cmd/commands/dev.go
@@ -17,7 +17,7 @@ func init() {
 		PreRun: setLogLevel,
 	}
 
-	devLocal := &cobra.Command{
+	devLocalUp := &cobra.Command{
 		Use:    "local",
 		Short:  "Run local development server",
 		Long:   `Run a local development server`,
@@ -26,10 +26,22 @@ func init() {
 		PreRun: setLogLevel,
 	}
 
+	devLocalDown := &cobra.Command{
+		Use:    "down",
+		Short:  "Stop local development server",
+		Long:   `Stop the local development server`,
+		RunE:   dev.StopLocal,
+		Args:   cobra.NoArgs,
+		PreRun: setLogLevel,
+	}
+
 	rootCmd.AddCommand(devCmd)
-	devCmd.AddCommand(devLocal)
-	devLocal.PersistentFlags().
+	devCmd.AddCommand(devLocalUp)
+	devLocalUp.PersistentFlags().
 		BoolVar(&dev.Debug, "debugger", false, "Enable debugger startup")
-	devLocal.PersistentFlags().
+	devLocalUp.PersistentFlags().
 		BoolVar(&dev.Live, "live", false, "Enable hot reload")
+	devCmd.AddCommand(devLocalDown)
+	devLocalDown.PersistentFlags().
+		BoolVar(&dev.Debug, "debugger", false, "Enable debugger startup")
 }
--- pkg/dev/dev.go
+++ pkg/dev/dev.go
@@ -56,3 +56,37 @@ func StartLocal(cmd *cobra.Command, args []string) error {
 
 	return nil
 }
+
+// StopLocal stops the live development server.
+func StopLocal(cmd *cobra.Command, args []string) error {
+	slog.Info("Starting local development server")
+
+	binary, err := exec.LookPath("docker")
+	if err != nil {
+		return fmt.Errorf("docker binary not found: %w", err)
+	}
+
+	profile := "dev"
+	if Debug {
+		profile = "debug"
+	}
+
+	baseArgs := []string{
+		"compose",
+		"--profile",
+		profile,
+		"--file",
+		"./tool/dev/docker-compose.yaml",
+		"down",
+	}
+
+	slog.Debug("Running command", slog.Any("binary", binary), slog.Any("baseArgs", baseArgs))
+
+	// nosemgrep: go.lang.security.audit.dangerous-syscall-exec.dangerous-syscall-exec // exec.LookPath() is used to locate the binary via $PATH, however we run on trusted developer machines
+	err = syscall.Exec(binary, append([]string{binary}, baseArgs...), os.Environ())
+	if err != nil {
+		return fmt.Errorf("error running workflow ci command: %w", err)
+	}
+
+	return nil
+}
